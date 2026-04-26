94283f25917402cb920b80c3829953d6d313717c
Eduardo Borges de Souza <edardo.borges@sga.pucminas.br>
2026-04-26 00:40:12 -0300

    AEDs2, TP2, Q3 e Q6

---
+++ AED's_II/TP2/Questão 3 - TP2/BaseQ3.c
@@ -0,0 +1,276 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MATRICULA "890309"
+
+int comp = 0;
+int mov  = 0;
+
+typedef struct Data {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct Hora {
+    int hora;
+    int minuto;
+} Hora;
+
+typedef struct Restaurante {
+    int id_restaurante;
+    char* nome;
+    char* cidade;
+    int capacidade;
+    double avaliacao;
+    char** tipo_cozinha;
+    int faixa_preco;
+    Hora hora_abertura;
+    Hora hora_fechamento;
+    Data data_abertura;
+    bool aberto;
+} Restaurante;
+
+typedef struct ColecaoRestaurante {
+    int tamanho;
+    Restaurante* restaurante;
+} Colecao_Restaurante;
+
+
+Data parse_data(char* s) {
+    Data d;
+    sscanf(s, "%d-%d-%d", &d.ano, &d.mes, &d.dia);
+    return d;
+}
+
+void formatar_data(Data* data, char* buffer) {
+    sprintf(buffer, "%02d/%02d/%04d", data->dia, data->mes, data->ano);
+}
+
+
+Hora parse_hora(char* s) {
+    Hora h;
+    sscanf(s, "%d:%d", &h.hora, &h.minuto);
+    return h;
+}
+
+void formatar_hora(Hora* hora, char* buffer) {
+    sprintf(buffer, "%02d:%02d", hora->hora, hora->minuto);
+}
+
+
+void liberar_restaurante(Restaurante* r) {
+    if (r == NULL) return;
+    free(r->nome);
+    free(r->cidade);
+    if (r->tipo_cozinha != NULL) {
+        free(r->tipo_cozinha[0]);
+        free(r->tipo_cozinha);
+    }
+}
+
+Restaurante* parse_restaurante(char* s) {
+    Restaurante* r = (Restaurante*) malloc(sizeof(Restaurante));
+    if (r == NULL) {
+        printf("Nao foi possivel criar restaurante!\n");
+        return NULL;
+    }
+    char hora_a[6], hora_f[6], data_a[11];
+    char nome[100], cidade[100], preco[10], tipo[40], aberto[10];
+
+    sscanf(s, "%d,%[^,],%[^,],%d,%lf,%[^,],%[^,],%[^-]-%[^,],%[^,],%[^\n]",
+           &r->id_restaurante, nome, cidade, &r->capacidade,
+           &r->avaliacao, tipo, preco, hora_a, hora_f,
+           data_a, aberto);
+
+    for (int i = 0; aberto[i] != '\0'; i++) {
+        if (aberto[i] == '\r' || aberto[i] == '\n' || aberto[i] == ' ')
+            aberto[i] = '\0';
+    }
+    r->aberto = (strcmp(aberto, "true") == 0);
+    r->hora_abertura   = parse_hora(hora_a);
+    r->hora_fechamento = parse_hora(hora_f);
+    r->data_abertura   = parse_data(data_a);
+
+    int tam;
+
+    tam = 0; while (nome[tam] != '\0') tam++;
+    r->nome = (char*) malloc((tam + 1) * sizeof(char));
+    sprintf(r->nome, "%s", nome);
+
+    tam = 0; while (cidade[tam] != '\0') tam++;
+    r->cidade = (char*) malloc((tam + 1) * sizeof(char));
+    sprintf(r->cidade, "%s", cidade);
+
+    tam = 0; while (preco[tam] != '\0') tam++;
+    r->faixa_preco = tam;
+
+    tam = 0; while (tipo[tam] != '\0') tam++;
+    for (int i = 0; tipo[i] != '\0'; i++)
+        if (tipo[i] == ';') tipo[i] = ',';
+    r->tipo_cozinha = (char**) malloc(1 * sizeof(char*));
+    r->tipo_cozinha[0] = (char*) malloc((tam + 1) * sizeof(char));
+    sprintf(r->tipo_cozinha[0], "%s", tipo);
+
+    return r;
+}
+
+void formatar_restaurante(Restaurante* restaurante, char* buffer) {
+    char hora_abertura[7], hora_fechamento[7], data_abertura[12], str_aberto[6];
+    formatar_hora(&restaurante->hora_abertura, hora_abertura);
+    formatar_hora(&restaurante->hora_fechamento, hora_fechamento);
+    formatar_data(&restaurante->data_abertura, data_abertura);
+
+    char f_preco[5];
+    int i;
+    for (i = 0; i < restaurante->faixa_preco; i++) f_preco[i] = '$';
+    f_preco[i] = '\0';
+
+    if (restaurante->aberto) sprintf(str_aberto, "true");
+    else                     sprintf(str_aberto, "false");
+
+    sprintf(buffer,
+        "[%d ## %s ## %s ## %d ## %.1lf ## [%s] ## %s ## %s-%s ## %s ## %s]",
+        restaurante->id_restaurante, restaurante->nome, restaurante->cidade,
+        restaurante->capacidade, restaurante->avaliacao,
+        restaurante->tipo_cozinha[0], f_preco,
+        hora_abertura, hora_fechamento, data_abertura, str_aberto);
+}
+
+
+void ler_csv_colecao(Colecao_Restaurante* colecao, char* path) {
+    FILE* arq = fopen(path, "r");
+    if (arq == NULL) {
+        printf("Erro ao abrir arquivo!\n");
+        return;
+    }
+    char linha[200];
+    fgets(linha, sizeof(linha), arq); // cabecalho
+    int i = 0;
+    while (fgets(linha, sizeof(linha), arq) != NULL) {
+        Restaurante* aux = parse_restaurante(linha);
+        colecao->restaurante[i] = *aux;
+        i++;
+        free(aux);
+    }
+    fclose(arq);
+}
+
+Colecao_Restaurante* ler_csv() {
+    FILE* arq = fopen("/tmp/restaurantes.csv", "r");
+    if (arq == NULL) {
+        printf("Erro ao abrir arquivo!\n");
+        return NULL;
+    }
+    int tam = 0;
+    char linha[200];
+    while (fgets(linha, sizeof(linha), arq) != NULL) tam++;
+    fclose(arq);
+
+    Colecao_Restaurante* novaCole = (Colecao_Restaurante*) malloc(sizeof(Colecao_Restaurante));
+    if (novaCole == NULL) {
+        printf("Erro ao alocar Colecao!\n");
+        return NULL;
+    }
+    novaCole->tamanho = tam - 1;
+    novaCole->restaurante = (Restaurante*) malloc((tam - 1) * sizeof(Restaurante));
+    if (novaCole->restaurante == NULL) {
+        printf("Erro ao alocar restaurante!\n");
+        free(novaCole);
+        return NULL;
+    }
+    ler_csv_colecao(novaCole, "/tmp/restaurantes.csv");
+    return novaCole;
+}
+
+
+int buscar_id(Colecao_Restaurante* colecao, int id_buscado) {
+    for (int i = 0; i < colecao->tamanho; i++) {
+        if (colecao->restaurante[i].id_restaurante == id_buscado) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int transformar_int(char* s) {
+    int qtd;
+    for (qtd = 0; s[qtd] != '\0' && s[qtd] != '\n' && s[qtd] != '\r'; qtd++);
+    int sinal = 1, ini = 0;
+    if (s[0] == '-') { sinal = -1; ini = 1; }
+    int resposta = 0, contador = 1;
+    for (int i = qtd - 1; i >= ini; i--) {
+        resposta += (s[i] - '0') * contador;
+        contador *= 10;
+    }
+    return resposta * sinal;
+}
+
+
+void selecao_nome(Restaurante* array, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int menor = i;
+        for (int j = i + 1; j < n; j++) {
+            comp++;
+            if (strcmp(array[j].nome, array[menor].nome) < 0) {
+                menor = j;
+            }
+        }
+        if (menor != i) {
+            Restaurante aux  = array[i];
+            array[i]         = array[menor];
+            array[menor]     = aux;
+            mov += 3;
+        }
+    }
+}
+
+int main() {
+    Colecao_Restaurante* cr = ler_csv();
+    if (cr == NULL) return 1;
+
+    Restaurante* selecionados = (Restaurante*) malloc(cr->tamanho * sizeof(Restaurante));
+    int n_sel = 0;
+
+    char linha[32];
+    while (fgets(linha, sizeof(linha), stdin) != NULL) {
+        if (linha[0] == '\n' || linha[0] == '\r' || linha[0] == '\0') continue;
+        if (linha[0] == '-' && linha[1] == '1') break;
+
+        int id  = transformar_int(linha);
+        int pos = buscar_id(cr, id);
+        if (pos != -1) {
+            selecionados[n_sel] = cr->restaurante[pos];
+            n_sel++;
+        }
+    }
+
+    clock_t inicio = clock();
+    selecao_nome(selecionados, n_sel);
+    clock_t fim = clock();
+    double total_tempo = ((fim - inicio) / (double) CLOCKS_PER_SEC) * 1000.0;
+
+    char buffer[512];
+    for (int i = 0; i < n_sel; i++) {
+        formatar_restaurante(&selecionados[i], buffer);
+        printf("%s\n", buffer);
+    }
+
+    FILE* arq_log = fopen(MATRICULA "_selecao.txt", "w");
+    if (arq_log != NULL) {
+        fprintf(arq_log, "%s\t%d\t%d\t%.4lf\n",
+                MATRICULA, comp, mov, total_tempo);
+        fclose(arq_log);
+    }
+
+    free(selecionados);
+    for (int i = 0; i < cr->tamanho; i++) {
+        liberar_restaurante(&cr->restaurante[i]);
+    }
+    free(cr->restaurante);
+    free(cr);
+    return 0;
+}
