8739df52fa7f376778d3144044f9916c637cb8f0
Eduardo Borges de Souza <edardo.borges@sga.pucminas.br>
2026-06-25 07:07:04 -0300

    atividade 1 hash

--- AEDs_II/EstudosAEDs2/TabelaHash/hashMinha.java
+++ AEDs_II/EstudosAEDs2/TabelaHash/hashMinha.java
@@ -28,6 +28,53 @@ public class Pessoa{
     }
 }
 public class hashMinha {
-    
-    
+    private Pessoa[] tabela;
+    public hashMinha(){
+        tabela = new Pessoa[366];
+    }
+    public void inserir(Pessoa p){
+        int mes [] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int cont= 0;
+        for(int i = 0; i < p.getMes(); i++){
+            cont += mes[i];
+        }
+        cont += p.getDia();
+        cont--;
+        tabela[cont] = p;
+    }
+    public Pessoa pesquisar(int dia, int mes, String nome){
+         int meses [] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int cont= 0;
+        for(int i = 0; i < mes; i++){
+            cont += meses[i];
+        }
+        cont += dia;
+        cont--;
+        if(tabela[cont] != null && tabela[cont].getNome().equals(nome)){
+            return tabela[cont];
+        }else{
+            System.out.println("Pessoa não encontrada");
+            return null;
+        }
+    }
+    public Pessoa remover(int dia, int mes, String nome){
+         int meses [] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int cont= 0;
+        for(int i = 0; i < mes; i++){
+            cont += meses[i];
+        }
+        cont += dia;
+        cont--;
+        if(tabela[cont] != null && tabela[cont].getNome().equals(nome)){
+            System.out.println(tabela[cont].getNome()+ " removido com sucesso");
+            Pessoa p = tabela[cont];
+            tabela[cont] = null;
+            return p;
+        }else{
+            System.out.println("Pessoa não encontrada");
+            return null;
+        }
+    }
+
 }
+
